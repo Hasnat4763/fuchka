bbae1604c651fedd6c2863ebec261063e1cb223e
Hasnat4763 <abulhasnat5737@gmail.com>
2025-11-10 19:01:51 +0600

    Putting some basic stuff and tinkering with TWCSS and Svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<head>
+        <title>Fuchka</title>
+</head>
+<body class = "bg-white-500">
+    <div class = "md:container sm:text-1xl sm:text-left md:mx-auto p-4 md:text-center text-black md:text-2xl">
+        <h1 class = "md:text-4xl pb-2">Welcome here the wanderer of the internet</h1>
+       <p class = "pb-2">Here I introduce you with Fuchka</p>
+       <p>Fuchka is a very Famous street food from South Asia.</p>
+       <p>It is a deep fried hollow spherical shell filled with many type of fillings and spices</p>
+       <p>It is known by very different names in different regions.</p>
+       <p>Like in Bangladesh and India's West Bengal, People know it as Fuchka, But in other areas of India they may know it as Golgappa or Pani Puri.</p>
+       <p>But it is the same food and is loved by everyone.</p>
+       <p>Mainly girls love fuchka a lot compared to boys.</p>
+    </div>
+<div class = "md:flex md:justify-center gap-6 p-4">
+    <div class="flex-initial">
+        <img class = "w-64 h-64" src="https://images.squarespace-cdn.com/content/v1/5ea5f3913b0ccf06d0ec2563/1593216324212-K527RACGDU12BTJH1AZW/Fuchka+%282%29.jpg?format=2500w" alt = "Fuchka">
+        <p>Fuchka from Bangladesh</p>
+    </div>
+    <div class= "flex-initial">
+        <img class="w-64 h-64" alt ="Golgappa" src="https://images.archanaskitchen.com/images/recipes/snack-recipes/indian-snack-recipes/Gol_Gappa_Chaat_93ded01e51.jpg">
+        <p>Golgappa from India</p>
+    </div>
+    
+</div>
+
+</body>
